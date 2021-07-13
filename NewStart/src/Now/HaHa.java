0c7a366564558d64a08128241015fecc4f8eb0dd
HP <HP@DESKTOP-FCAPC45>
2021-07-13 13:54:56 +0900

    start git

---
+++ NewStart/src/Now/HaHa.java
@@ -0,0 +1,24 @@
+package Now;
+
+public class HaHa {
+
+	HaHa(){
+		
+	}
+	
+	public static int numbering(int num) {
+		int i = 0;
+		int sum = 0;
+		while(i<num) {
+			sum += i;
+			i ++;
+		}
+		return sum;
+	}//메소드 정의
+	
+	
+}
+
+	//메소드 호출
+//String[]args = main문을 나타냄!
+//굳이 정의를 하지 않아도 호출문에 내용을 쓰면 실행과 출력이 가능하다
---
+++ NewStart/src/Now/HaHaMain.java
@@ -0,0 +1,12 @@
+package Now;
+
+public class HaHaMain {
+
+	public static void main(String[] args) {
+			System.out.println(HaHa.numbering(6));
+			int a= HaHa.numbering(3);
+			HaHa.numbering(2);
+			HaHa.numbering(12);
+	}
+
+}
